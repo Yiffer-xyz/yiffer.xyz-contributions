8254ff0334bcfe100b94c8278a685947cbe7a463
Ragnar Bråstein <ragnarob@protonmail.com>
2024-10-17 01:08:11 +0100

    root back

--- app/root.tsx
+++ app/root.tsx
@@ -201,20 +201,20 @@ function Layout({
   );
 }
 
-// export function ErrorBoundary() {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <meta charSet="utf-8" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <Meta />
-//         <Links />
-//       </head>
-//       <body className="dark:bg-bgDark text-text-light dark:text-text-dark">
-//         <Layout frontPageUrl={'https://new.testyiffer.xyz'} user={null} excludeLogin>
-//           <YifferErrorBoundary />
-//         </Layout>
-//       </body>
-//     </html>
-//   );
-// }
+export function ErrorBoundary() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="dark:bg-bgDark text-text-light dark:text-text-dark">
+        <Layout frontPageUrl={'https://new.testyiffer.xyz'} user={null} excludeLogin>
+          <YifferErrorBoundary />
+        </Layout>
+      </body>
+    </html>
+  );
+}
